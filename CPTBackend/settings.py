98cf67a141b5d00261442e98c39940b2ca6fb9fc
Harry Lee <harrileeeee@gmail.com>
2024-08-08 20:20:05 -0700

    cicd: remove csrf middleware

--- CPTBackend/settings.py
+++ CPTBackend/settings.py
@@ -81,6 +81,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ["https://shnyu.danlanlove.com"]
+
 ROOT_URLCONF = 'CPTBackend.urls'
 
 TEMPLATES = [
